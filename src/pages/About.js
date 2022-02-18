410193d17e1b954b57952460c95f89330bb67afe
ShahidKhan3464 <shahidafridi26360@gmail.com>
2022-02-18 10:07:27 -0800

    added files

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, porro id ipsum consequatur libero, ipsa dolor consectetur, nihil veritatis ut modi commodi nulla. Odio quas possimus ut soluta, vero reprehenderit?
+      </p>
+    </section>
+  )
+}
+
+export default About
